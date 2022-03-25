43000c4f05f37d7c8660e61951a1e0cc554058f8
Coder31Sayantan <sayantansadhukhan46@gmail.com>
2022-03-25 22:58:12 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("txt-input");
+
+function clickHandler() {
+
+}
+
+btnTranslate.addEventListener("click", function clickHandler)
