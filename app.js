77bcd70a642ed1a131d2166ce46770cebeeb7ef8
Coder31Sayantan <sayantansadhukhan46@gmail.com>
2022-03-26 13:51:10 +0530

    translator fixed

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("txt-input");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler() {
-
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
 
-btnTranslate.addEventListener("click", function clickHandler)
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Server down! Try again later");
+}
+
+function clickHandler() {
+    var inputText = txtInput.ariaValueMax; //taking input
+
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
